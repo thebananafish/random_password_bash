813b999cace14fefaffcde41bd5d2b1791bddb64
benwaffle <iofelben@gmail.com>
2013-10-14 08:45:36 -0400

    added comments

--- passwd_change.sh
+++ passwd_change.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-PASS_FILE=animals.txt
-USER=test
-EMAIL=test@example.com
+PASS_FILE=animals.txt # the file from which to get passwords
+USER=test # the username who's password is to be changed
+EMAIL=test@example.com # the email to send the new password too
 
-if [ -e ./$PASS_FILE ]; 
+if [ -e ./$PASS_FILE ]; then # if the file name in PASS_FILE above exists
 
-then
+    NEW_PASS=`sort -R $PASS_FILE | head -n 1` # sort the file randomly and set NEW_PASS to one of the passwords
+    echo "$NEW_PASS" | mail -s 'Monthly Password Change' $EMAIL # send the new password to the email
+    echo -e "$NEW_PASS\n$NEW_PASS" | passwd $USER # set the new password
 
-    NEW_PASS=`sort -R $PASS_FILE | head -n 1`
-    echo "$NEW_PASS" | mail -s 'Monthly Password Change' $EMAIL
-    echo -e "$NEW_PASS\n$NEW_PASS" | passwd $USER
+else # if the file in PASS_FILE does not exist
 
-else
-        echo "password text file not found"
-    fi
-
+    echo "password text file not found" # print out an error message
+    
+fi # end the conditional
